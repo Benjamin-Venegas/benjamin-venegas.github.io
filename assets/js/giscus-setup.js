ba8403011b958c54a1833bfbfba5235e2d18f2ab
Benjamin-Venegas <benjavenegas2011@gmail.com>
2026-01-05 22:51:08 +0000

    Deploying to gh-pages from @ Benjamin-Venegas/BenjaminVenegas.github.io@686c8486a798ca8c253c44ab1d250833ef8a06fb 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Benjamin-Venegas/-Benjamin-Venegas.github.io",
+    "data-repo": "Benjamin-Venegas/BenjaminVenegas.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
