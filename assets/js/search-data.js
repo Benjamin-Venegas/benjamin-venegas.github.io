455bc3a4aaf522e45faed0eaf36105e6811aa6ec
Benjamin-Venegas <benjavenegas2011@gmail.com>
2026-01-23 19:26:48 +0000

    Deploying to gh-pages from @ Benjamin-Venegas/benjamin-venegas.github.io@4b6e1f0d648cf7b1086821fa125bfd1372b12890 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-notes",
           title: "Notes",
-          description: "/",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/notes/";
