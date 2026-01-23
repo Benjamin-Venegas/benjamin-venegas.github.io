c70d76f732c8a917b72eaebb8ddc595390f4c480
Benjamin-Venegas <benjavenegas2011@gmail.com>
2026-01-23 20:52:53 +0000

    Deploying to gh-pages from @ Benjamin-Venegas/benjamin-venegas.github.io@abad49fd8d29563683dfecd6e9ec52b5601ef225 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -480,14 +480,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%62%65%76%65%6E%65%67%61%73%32%30%32%30@%75%64%65%63.%63%6C", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -495,6 +495,20 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0004-6803-5784", "_blank");
+        },
+      },{
+        id: 'social-mathscinet',
+        title: 'Mathscinet',
+        section: 'Socials',
+        handler: () => {
+          window.open("", "_blank");
         },
       },{
         id: 'social-rss',
