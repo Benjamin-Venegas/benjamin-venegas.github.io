6c8ec6c124b0e1f873f579a6b0d996346032dd42
Benjamin-Venegas <benjavenegas2011@gmail.com>
2026-03-17 22:50:52 +0000

    Deploying to gh-pages from @ Benjamin-Venegas/benjamin-venegas.github.io@5052238659a6b929f8d4de05709355e879e989f5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -15,6 +15,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
+          },
+        },{id: "nav-research",
+          title: "research",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/index.html";
           },
         },{id: "nav-cv",
           title: "cv",
