849f79e3ee5a855b18d7db540040c0b4cb89fc7e
Benjamin-Venegas <benjavenegas2011@gmail.com>
2026-03-21 19:08:19 +0000

    Deploying to gh-pages from @ Benjamin-Venegas/benjamin-venegas.github.io@bc1cac1c62f6e7b68504115bd3f0f039ea455696 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -496,7 +496,7 @@ ninja.data = [{
         title: 'ResearchGate',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.researchgate.net/profile/(https://www.researchgate.net/profile/Benjamin-Venegas-5?ev=hdr_xprf)/", "_blank");
+          window.open("https://www.researchgate.net/profile/Benjamín-Venegas/", "_blank");
         },
       },{
         id: 'social-rss',
